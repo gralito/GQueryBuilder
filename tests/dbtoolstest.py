b64521827b3ed832b50b30d84e2d63a00220a7b5
gralito <gralito@protonmail.com>
2026-05-12 17:49:48 +0200

    test dbtools functions

---
+++ tests/dbtoolstest.py
@@ -0,0 +1,14 @@
+import unittest
+
+import utils.dbtools as dbt
+
+class DbtoolsTest(unittest.TestCase):
+    def testOr(self):
+        a = "A"
+        b = "B"
+        self.assertEqual(dbt.build_or(a, b), 'A OR B')
+
+    def testAnd(self):
+        a = "A"
+        b = "B"
+        self.assertEqual(dbt.build_and(a, b), 'A AND B')
--- utils/dbtools.py
+++ utils/dbtools.py
@@ -1,5 +1,5 @@
 
 
-def build_or(a: str, b: str): return f"{a} OR {B}"
+def build_or(a: str, b: str): return f"{a} OR {b}"
 
-def build_and(a: str, b: str): return f"{a} AND {B}"
+def build_and(a: str, b: str): return f"{a} AND {b}"
