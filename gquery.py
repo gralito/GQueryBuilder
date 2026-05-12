3d8d3c99fae1d4e2876998ef2722b5a9c1d1483a
gralito <gralito@protonmail.com>
2026-05-12 17:37:02 +0200

    update GQuery + tests
    create where function
    create tests OK

--- gquery.py
+++ gquery.py
@@ -2,32 +2,48 @@
 
 
 class GQuery:
-    parts = []
-    _select = []
-    _from_table: str
-    _where = []
-    _group = []
-    _order: str
-    _limit: str
-    
-    def from_table(self, table: str)->GQuery:
-        self._from_table = table
-        return self
+    _select: str = ""
+    _table: str = ""
+    _where: str = ""
+    # _group: str
+    # _order: str
+    # _limit: str
     
     def select(self, *args)->GQuery:
         self._select = ', '.join(args)
         return self
-        
+    
+    def table(self, table: str)->GQuery:
+        self._table = table
+        return self
+    
+    def where(self, condition: str)->GQuery:
+        self._where = condition
+        return self
+    
+    
     def gquery(self)->str: 
-        self.parts = ['SELECT']
+        """
+        gquery build the SQLite3 query from the GQuery object.
+
+        Returns:
+            str: The SQLite3 query that can be send to any ORM.
+        """
+        parts = []
+        parts.append('SELECT')
         if self._select:
-            self.parts.append(self._select)
+            parts.append(self._select)
         else:
-            self.parts += ['*']
+            parts += ['*']
         
-        self.parts.append('FROM')
-        self.parts.append(self._from_table)
-        return ' '.join(self.parts)     
+        parts.append('FROM')
+        parts.append(self._table)
+        
+        if self._where:
+            parts.append('WHERE')
+            parts.append(self._where)
+        
+        return ' '.join(parts)     
             
 
 if __name__ == "__main__":
--- tests/gquerytest.py
+++ tests/gquerytest.py
@@ -4,19 +4,25 @@ from gquery import GQuery
 
 class GQueryTest(unittest.TestCase):
     def testSimpleQuery(self):
-        q = GQuery().from_table('posts').select('name')
+        q = GQuery().table('posts').select('name')
         query = q.gquery()
         self.assertEqual(str(query),
                           'SELECT name FROM posts')
         
-    def testSimpleQuerySelectSeveral(self):
-        q = GQuery().from_table('posts').select('name', 'value', 'date')
+    def testSimpleQuerySelectSeveralFields(self):
+        q = GQuery().table('posts').select('name', 'value', 'date')
         query = q.gquery()
         self.assertEqual(str(query),
                          "SELECT name, value, date FROM posts")
     
     def testSimpleQuerySelectAll(self):
-        q = GQuery().from_table('posts')
+        q = GQuery().table('posts')
         query = q.gquery()
         self.assertEqual(str(query),
                          'SELECT * FROM posts')
+        
+    def testSelectWhereQuery(self):
+        q = GQuery().select('name').table('posts').where('category = livre')
+        query = q.gquery()
+        self.assertEqual(str(query),
+                         'SELECT name FROM posts WHERE category = livre')
