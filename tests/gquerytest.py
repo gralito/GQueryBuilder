9492e4b39e616ecd35cd1c957bbb768be0e87461
gralito <gralito@protonmail.com>
2026-05-21 21:21:27 +0200

    refactor test files
    use snake_case

--- tests/gquerytest.py
+++ tests/gquerytest.py
@@ -5,18 +5,19 @@ import utils.dbtools as dbt
 
 class GQueryTest(unittest.TestCase):
     
-    def testSimpleAlias(self):
+    def test_simple_alias(self):
         q = GQuery().table(('posts', 'p'))
         self.assertEqual(str(q._table),
                          'FROM posts as p')
     
-    def testSimpleWhereQuery(self):
+    def test_simple_where(self):
         q = GQuery().where('a = :a')
         self.assertEqual(q._where,
                          'WHERE a = :a')
         
-    def testComplexWhereQuery(self):
+    def test_complex_where(self):
         q = GQuery().where(dbt.build_and(dbt.build_or("a = :a", "b = :b"), "c = :c"))
         self.assertEqual(str(q._where),
                          "WHERE ((a = :a OR b = :b) AND c = :c)")
         
+     
--- tests/readquerytest.py
+++ tests/readquerytest.py
@@ -4,30 +4,39 @@ from src.readquery import ReadQuery
 
 class ReadQueryTest(unittest.TestCase):
     
-    def testSimpleSelectQuery(self):
+    def test_simple_select_query(self):
         q = ReadQuery()
         q.table(('posts',)).select('name')
         query = q.build_query()
         self.assertEqual(str(query),
                          'SELECT name FROM posts')
          
-    def testSimpleQuerySelectSeveralFields(self):
+    def test_simple_query_select_several_fields(self):
         q = ReadQuery().table(('posts',))
         q.select('name', 'value', 'date')
         query = q.build_query()
         self.assertEqual(str(query),
                          "SELECT name, value, date FROM posts")
         
-    def testSimpleQuerySelectAllFieldsCallingMethod(self):
+    def test_simple_suery_select_all_fields_calling_method(self):
         q = ReadQuery().table(('posts',))
         q.select()
         query = q.build_query()
         self.assertEqual(str(query),
                          'SELECT * FROM posts')
             
-    def testSimpleQuerySelectAllFields(self):
+    def test_simple_query_select_all_fields(self):
         q = ReadQuery()
         q.table(('posts',))
         query = q.build_query()
         self.assertEqual(str(query),
                          'SELECT * FROM posts') 
+        
+    def test_select_all_fields_with_alias(self):
+        q = ReadQuery()
+        q.table(('posts', 'p'), ('blog', 'b'))
+        query = q.build_query()
+        self.assertEqual(str(query),
+                         "SELECT * FROM posts as p, blog as b")
+    
+    
