31057ae496d04764e6aa162664ec7030e20d43cb
gralito <gralito@protonmail.com>
2026-05-21 22:22:50 +0200

    resolve conflicts

--- tests/readquerytest.py
+++ tests/readquerytest.py
@@ -39,13 +39,9 @@ class ReadQueryTest(unittest.TestCase):
         self.assertEqual(str(query),
                          "SELECT * FROM posts as p, blog as b")
     
-<<<<<<< HEAD
     def test_simple_query_with_distinct(self):
         q = ReadQuery().select('username', distinct=True)
         q.table(('posts', 'p'))
         query = q.build_query()
         self.assertEqual(str(query),
                          "SELECT DISTINCT username FROM posts as p")
-=======
-    
->>>>>>> feature/ReadQuery
