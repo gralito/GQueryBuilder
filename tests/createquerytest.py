9584af895c8041268d9869f7cd0a5c8df59b5c71
gralito <gralito@protonmail.com>
2026-05-24 00:22:27 +0200

    update tests
    update function setUp()

--- tests/createquerytest.py
+++ tests/createquerytest.py
@@ -6,28 +6,26 @@ from src.GQueryBuilder.createquery import CreateQuery
 class CreateQueryTest(unittest.TestCase):
     
     def setUp(self):
-        self.database = 'db.sqlite3'
+        self.query = CreateQuery('db.sqlite3')
     
     def test_simple_query(self):
-        q = CreateQuery(self.database).table(('posts',))
-        q.target(['name']).values(['julien'])
-        q.build_query()
-        self.assertEqual(q._query,
+        self.query.table(('posts',))
+        self.query.target(['name']).values(['julien'])
+        self.query.build_query()
+        self.assertEqual(self.query._query,
                          "INSERT INTO posts (name) VALUES ('julien')")
         
     def test_multiple_values(self):
-        q = CreateQuery(self.database)
-        q.table(('posts', 'p'))
-        q.target(['name', 'age'])
-        q.values(['julien', 43])
-        q.build_query()
-        self.assertEqual(q._query,
+        self.query.table(('posts', 'p'))
+        self.query.target(['name', 'age'])
+        self.query.values(['julien', 43])
+        self.query.build_query()
+        self.assertEqual(self.query._query,
                          "INSERT INTO posts as p (name, age) VALUES ('julien', 43)")
     
     def test_default_values(self):
-        q = CreateQuery(self.database)
-        q.table(('posts',))
-        q.target(['name'])
-        q.build_query()
-        self.assertEqual(q._query,
+        self.query.table(('posts',))
+        self.query.target(['name'])
+        self.query.build_query()
+        self.assertEqual(self.query._query,
                          "INSERT INTO posts (name) DEFAULT VALUES")
