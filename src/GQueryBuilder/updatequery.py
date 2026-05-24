7645716fdb7ae95ef4e0439f320aeddad2e267a2
gralito <gralito@protonmail.com>
2026-05-24 22:28:01 +0200

    create UpdateQuery
    create class
    create method expressions()
    create build_query()
    run simple test OK
    update CreateQuery - __init__

--- src/GQueryBuilder/updatequery.py
+++ src/GQueryBuilder/updatequery.py
@@ -2,4 +2,34 @@ from src.GQueryBuilder.gquery import GQuery
 
 
 class UpdateQuery(GQuery):
-    pass
+    def __init__(self, database):
+        super().__init__(database)
+        self._expressions = ""
+    
+    def expressions(self, expressions: dict):
+        result = []
+        for key, value in expressions.items():
+            if type(value) == str:
+                str_value = f"'{value}'"
+            else :
+                str_value = f"{value}"
+            result.append(f"{key} = {str_value}")
+        self._expressions = ", ".join(result)
+        return self
+        
+    
+    def build_query(self)->str:
+        """
+        build the SQLite SELECT query using the instance attributes.
+
+        Returns:
+            str: the SQLite query, ready to use.
+        """
+        parts = []
+        parts.append("UPDATE")
+        parts.append(self._table)
+        parts.append("SET")
+        parts.append(self._expressions)
+        if self._where: parts.append(self._where)
+        
+        self._build_query(parts)
--- tests/updatequerytest.py
+++ tests/updatequerytest.py
@@ -4,4 +4,15 @@ from src.GQueryBuilder.updatequery import UpdateQuery
 
 
 class UpdateQueryTest(unittest.TestCase):
-    pass
+    
+    def setUp(self):
+        self.database = "db.sqlite3"
+        
+    def test_simple_query(self):
+        q = UpdateQuery(self.database)
+        q.table(('posts',))
+        changes = {'name': 'gralito'}
+        q.expressions(changes)
+        q.build_query()
+        self.assertEqual(q._query,
+                         "UPDATE posts SET name = 'gralito'")
